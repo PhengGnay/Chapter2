6bfa2bd69116e66b4b5c920524c7d092e807e3e4
Pheng <phengyangster9@gmail.com>
2022-03-08 13:22:01 -0800

    adding chapter 2.3 and 2.4

---
+++ 2_4.py
@@ -0,0 +1,16 @@
+# num1 = 1
+# num2 = 10
+# num3 = 1000
+# sum_of_all_numbers = num1 + num2 + num3
+# print("Sum is {0:n}".format (sum_of_all_numbers))
+
+# list_of_numbers = [1, 10, 1000]
+# print(sum(list_of_numbers)) #sum
+# print(min(list_of_numbers)) #min
+# print(max(list_of_numbers)) #max
+# print(len(list_of_numbers)) #len
+# print(list_of_numbers.count(4)) #count
+# print(list_of_numbers.index(1)) #index
+# print(list_of_numbers.reverse()) #reverse
+
+
