90410af7430995bd4772685034eb386375c88462
Pheng <phengyangster9@gmail.com>
2022-03-08 14:22:26 -0800

    chapter_2_4 changes

--- 2_4.py
+++ 2_4.py
@@ -4,13 +4,13 @@
 # sum_of_all_numbers = num1 + num2 + num3
 # print("Sum is {0:n}".format (sum_of_all_numbers))
 
-# list_of_numbers = [1, 10, 1000]
-# print(sum(list_of_numbers)) #sum
-# print(min(list_of_numbers)) #min
-# print(max(list_of_numbers)) #max
-# print(len(list_of_numbers)) #len
-# print(list_of_numbers.count(4)) #count
-# print(list_of_numbers.index(1)) #index
-# print(list_of_numbers.reverse()) #reverse
+list_of_numbers = [1, 10, 1000]
+print(sum(list_of_numbers)) #sum
+print(min(list_of_numbers)) #min
+print(max(list_of_numbers)) #max
+print(len(list_of_numbers)) #len
+print(list_of_numbers.count(4)) #count
+print(list_of_numbers.index(1)) #index
+print(list_of_numbers.reverse()) #reverse
 
 
